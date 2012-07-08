aa833c0f94bbb4cacd22fafc68550634becbb5e6
Juergen Hoetzel <juergen@archlinux.org>
2012-07-08 18:13:25 +0200

    Haskells parition function

--- src/pearls_clj/smallest_free_number_dc.clj
+++ src/pearls_clj/smallest_free_number_dc.clj
@@ -3,13 +3,21 @@
 (declare minfrom)
 
 (defn smallest-free-number [xs]
-  (minfrom 0 [count xs, xs]))
+  (minfrom 0 [(count xs) xs]))
+
+
+(defn partition-hsk [pred coll]
+  "Haskells partition function, not lazy"
+  (loop [xs [] ys[] coll coll]
+    (cond (not (seq coll)) [xs ys]
+          (pred (first coll)) (recur (conj xs (first coll)) ys (rest coll))
+          :else (recur xs (conj ys (first coll)) (rest coll)))))
 
 (defn minfrom [a [n xs]]
-  (let [b (quot (+ a 1 n) 2)
-        [us vs] (partition-by (partial >= b) xs)
-        m (count us)
-        ] (cond
-            (zero? n) a
-            (= m (- b a)) (recur b [(- n m) vs])
-            :else (recur a [m us]))))
+  (let [b (+ a 1 (quot n 2))
+        [us vs] (partition-hsk (partial > b) xs)
+        m (count us)]
+    (cond
+      (zero? n) a
+      (= m (- b a)) (recur b [(- n m) vs])
+      :else (recur a [m us]))))
